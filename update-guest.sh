3c6929450db88c6661002219bd59057d6276682a
Kangwei Ling <lingkangwei.kevin@gmail.com>
2019-03-25 19:47:31 -0400

    rsync modules

--- update-guest.sh
+++ update-guest.sh
@@ -1,11 +1,14 @@
 source config.sh
 
 # copy modules
-rsync -av ${MOD_PATH}/ root@${guest_ip}:/lib/modules/.
+rsync -av ${MOD_PATH}/lib/modules/${VERSION} root@${guest_ip}:/lib/modules/.
 
+cd ${LINUX_SRC}
 scp System.map root@${guest_ip}:~/.
 scp arch/x86/boot/bzImage root@${guest_ip}:~/vmlinuz-${VERSION}
 scp .config root@${guest_ip}:~/.
 
 ssh root@${guest_ip} "installkernel ${VERSION} vmlinuz-${VERSION} System.map"
 ssh root@${guest_ip} "update-grub"
+
+cd -
